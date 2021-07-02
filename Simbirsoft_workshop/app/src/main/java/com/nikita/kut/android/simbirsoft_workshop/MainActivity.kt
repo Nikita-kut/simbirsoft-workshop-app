35aac26566c9327de91c9b1b924bb2147a0b25df
Kutsyy Nikita <kutcyi.nikita@gmail.com>
2021-07-02 11:28:57 +0300

    main activity fix

--- Simbirsoft_workshop/app/src/main/java/com/nikita/kut/android/simbirsoft_workshop/MainActivity.kt
+++ Simbirsoft_workshop/app/src/main/java/com/nikita/kut/android/simbirsoft_workshop/MainActivity.kt
@@ -11,36 +11,3 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
-
-@RequiresApi(Build.VERSION_CODES.N)
-fun main() {
-
-    val student = CollectionsBlock.Student()
-
-    val listStudents = student.listStudent
-
-    listStudents.forEach { println(it) }
-
-    student.averageGrade(listStudents, CollectionsBlock.Student::getEngGrade).forEach { t, u ->
-        println("Course = $t, Eng average grade = $u")
-    }
-
-    student.averageGrade(listStudents, CollectionsBlock.Student::getMathGrade).forEach { t, u ->
-        println("Course = $t, Math average grade = $u")
-    }
-
-
-    fun replaceAllNumbers(text: String): String? {
-        var text = text
-        val digitsText =
-            arrayOf("Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine")
-        var c = '0'
-        while (c <= '9') {
-            text = text.replace(c.toString().toRegex(), digitsText[c - '0'] + ' ')
-            ++c
-        }
-        return text
-    }
-
-    println(replaceAllNumbers("Text132"))
-}
